5bc9317eaec62600fe8acdcef0b2abfdad1d1734
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-06-06 14:45:06 +0700

    add admin view

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+	public function dashboard_admin()
+	{
+		$this->load->view('admin/dashboard');
+    }
+    
+    public function dashboard_hrd()
+	{
+		$this->load->view('hrd/dashboard');
+    }
+    
+    public function dashboard_pegawai()
+	{
+		$this->load->view('pegawai/dashboard');
+	}
+}
