db6f9372cb2935f24080db27b3f6e3f555e60976
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-06-07 13:42:55 +0700

    add handler jam kerja  when no session

--- application/controllers/Jam_Kerja.php
+++ application/controllers/Jam_Kerja.php
@@ -5,17 +5,46 @@ class Jam_Kerja extends CI_Controller {
 
 	public function view_admin()
 	{
+		if ($this->session->userdata('logged_in') == true AND $this->session->userdata('id_user_level') == 1) {
+
 		$this->load->view('admin/jam_kerja');
+
+		}else{
+
+			$this->session->set_flashdata('loggin_no_session','loggin_no_session');
+			redirect('Login/index');
+
+		}
+		
     }
     
     public function view_hrd()
 	{
+
+		if ($this->session->userdata('logged_in') == true AND $this->session->userdata('id_user_level') == 2) {
+
 		$this->load->view('hrd/jam_kerja');
+
+		}else{
+
+			$this->session->set_flashdata('loggin_no_session','loggin_no_session');
+			redirect('Login/index');
+
+		}
     }
 
     public function view_pegawai()
 	{
+		if ($this->session->userdata('logged_in') == true AND $this->session->userdata('id_user_level') == 3) {
+
 		$this->load->view('pegawai/jam_kerja');
+
+		}else{
+
+			$this->session->set_flashdata('loggin_no_session','loggin_no_session');
+			redirect('Login/index');
+
+		}
     }
     
 }
