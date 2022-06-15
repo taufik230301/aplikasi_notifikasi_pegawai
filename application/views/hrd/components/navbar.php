2014df4bcc672bfb57b9c8d971cba91a5f13c6aa
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-06-16 05:36:46 +0700

    add setting akun in aplikasi

--- application/views/hrd/components/navbar.php
+++ application/views/hrd/components/navbar.php
@@ -15,10 +15,6 @@
             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown"
                 aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#!">Settings</a></li>
-                <li>
-                    <hr class="dropdown-divider" />
-                </li>
                 <li><a class="dropdown-item" href="<?=base_url();?>Login/log_out">Logout</a></li>
             </ul>
         </li>
