b5022321b23473794fe325c59d4e069e86fda07e
taufik230301 <51141717+taufik230301@users.noreply.github.com>
2022-07-22 21:34:41 +0700

    add settings akun in admin

--- application/views/hrd/settings.php
+++ application/views/hrd/settings.php
@@ -40,7 +40,7 @@
         <div id="layoutSidenav_content">
             <main>
                 <div class="container-fluid px-4">
-                    <h1 class="mt-4">Setting Akun</h1>
+                    <h1 class="mt-4">Settings Akun</h1>
                     <ol class="breadcrumb mb-4">
                         <li class="breadcrumb-item"><a href="<?=base_url();?>Dashboard/dashboard_hrd">Dashboard</a>
                         </li>
